5725a0163ee7ed8be1012dfb2a2ea62242f3007a
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-08-15 00:42:35 +0530

    Updated Cryptogram

--- Python/Cryptogram/engine.py
+++ Python/Cryptogram/engine.py
@@ -1,6 +1,5 @@
-__author__ = "github.com/raj-patra"
-
-import string, random, math, base64, binascii, warnings
+# Imports
+import base64, binascii, math, random, string, warnings, rsa
 
 from cryptography.fernet import Fernet
 from collections import deque
@@ -22,7 +21,7 @@ class Cypher:
             Strength - Low
             Key - No key required for encryption or decryption
 
-        caesar Cypher
+        Caesar Cypher
             About - Procedure includes shifting the dictionary.
             Strength - Low
             Key - <int> type key required for encryption or decryption
@@ -71,6 +70,21 @@ class Cypher:
             About - Procedure includes modifying xor cypher with base64 encoding scheme.
             Strength - High
             Key - <str> type key required for encryption or decryption
+
+        RSA Cypher
+            About - RSA (Rivest–Shamir–Adleman) is a public-key cryptosystem that is widely used for secure data transmission.
+            Strength - High
+            Key - Key generated automatically for encryption or decryption.
+
+        Pseudo Random Cypher
+            About - Procedure includes shuffling the dictionary.
+            Strength - High
+            Key - <int> type key required for encryption or decryption
+        
+        Vigenere Cypher
+            About - Procedure includes encrypting alphabetic text by using a random sample genrated from a seed.
+            Strength - High
+            Key - <int> type key required for encryption or decryption
 
         Note - Key provided/generated during encryption should be used for a successful decryption.
         """
@@ -122,7 +136,7 @@ class Cypher:
         if type(message) != str:
             raise TypeError("'str' object required as 'message' attribute for encryption or decryption")
 
-        return message[::-1], key
+        return (message[::-1], key)
 
     def caesar(self, message: str, encrypt=False, decrypt=False, key=None):
         """Procedure includes shifting the dictionary.
@@ -145,11 +159,11 @@ class Cypher:
 
         if encrypt:                
             encrypted_data = ''.join([rotated_symbols[self.__symbols__.index(sym)] for sym in message])
-            return encrypted_data, int(key)
+            return (encrypted_data, int(key))
 
         if decrypt:
             decrypted_data = ''.join([self.__symbols__[rotated_symbols.index(sym)] for sym in message])
-            return decrypted_data, int(key)
+            return (decrypted_data, int(key))
     
     def shifting_caesar(self, message: str, encrypt=False, decrypt=False, key=None):
         """Procedure includes shifting the dictionary continously.
@@ -175,7 +189,7 @@ class Cypher:
                 rotated_symbols.rotate(-int(key))
                 encrypted_data += rotated_symbols[self.__symbols__.index(sym)]
 
-            return encrypted_data, int(key)
+            return (encrypted_data, int(key))
 
         if decrypt:
             rotated_symbols = self.__symbols__.copy()
@@ -185,7 +199,7 @@ class Cypher:
                 rotated_symbols.rotate(-int(key))
                 decrypted_data += self.__symbols__[rotated_symbols.index(sym)]
 
-            return decrypted_data, int(key)
+            return (decrypted_data, int(key))
     
     def rot13(self, message: str, encrypt=False, decrypt=False, key=None):
         """Procedure includes shifting the dictionary by the value of 13.
@@ -204,11 +218,11 @@ class Cypher:
 
         if encrypt:                
             encrypted_data = ''.join([rotated_symbols[self.__symbols__.index(sym)] for sym in message])
-            return encrypted_data, 13
+            return (encrypted_data, 13)
 
         if decrypt:
             decrypted_data = ''.join([self.__symbols__[rotated_symbols.index(sym)] for sym in message])
-            return decrypted_data, 13
+            return (decrypted_data, 13)
 
     def transposition(self, message: str, encrypt=False, decrypt=False, key=None):
         """About - Procedure includes arranging the message row-wise, in a matrix and extracting column-wise.
@@ -234,7 +248,7 @@ class Cypher:
                     encrypted_data[col] += message[position]
                     position += int(key)
 
-            return ''.join(encrypted_data), int(key)
+            return (''.join(encrypted_data), int(key))
         
         if decrypt:
             columns = math.ceil(len(message) / int(key))
@@ -242,7 +256,7 @@ class Cypher:
             for i in range(columns):
                 decrypted_data[i] += message[i::columns]
 
-            return ''.join(decrypted_data), int(key)
+            return (''.join(decrypted_data), int(key))
 
     def base64(self, message: str, encrypt=False, decrypt=False, key=None):
         """About - Procedure includes standard text-to-binary encoding scheme.
@@ -258,10 +272,10 @@ class Cypher:
         """
         if encrypt:
             encrypted_data = base64.b64encode(message.encode('ascii'))
-            return encrypted_data.decode('ascii'), key
+            return (encrypted_data.decode('ascii'), key)
         if decrypt:
             decrypted_data = base64.b64decode(message.encode('ascii'))
-            return decrypted_data.decode('ascii'), key
+            return (decrypted_data.decode('ascii'), key)
 
     def xor(self, message: str, encrypt=False, decrypt=False, key=None):
         """About - Procedure includes applying xor operator on message and key..
@@ -282,12 +296,12 @@ class Cypher:
         if encrypt:
             data = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in zip(message, cycle(key)))
             encrypted_data = base64.b64encode(data.encode('ascii')).decode('ascii')
-            return encrypted_data, key
+            return (encrypted_data, key)
 
         if decrypt:
             data = base64.b64decode(message.encode('ascii')).decode('ascii')
             decrypted_data = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in zip(data, cycle(key)))
-            return decrypted_data, key
+            return (decrypted_data, key)
     
     def multiplicative(self, message: str, encrypt=False, decrypt=False, key=None):
         """About - Procedure includes modifying caesar cypher with multiplication operation.
@@ -307,7 +321,7 @@ class Cypher:
 
         if encrypt:
             encrypted_data = [self.__symbols__[(self.__symbols__.index(sym)*int(key))%len(self.__symbols__)] for sym in message]
-            return ''.join(encrypted_data), int(key)
+            return (''.join(encrypted_data), int(key))
         
         if decrypt:
             decrypted_data = ''
@@ -316,7 +330,7 @@ class Cypher:
                     if self.__symbols__[(self.__symbols__.index(og_sym)*int(key))%len(self.__symbols__)] == sym:
                         decrypted_data += og_sym
 
-            return decrypted_data, int(key)
+            return (decrypted_data, int(key))
 
     def monoalphabetic(self, message: str, encrypt=False, decrypt=False, key=None):
         """About - Procedure includes replacing original symbols with random symbols from key.
@@ -341,7 +355,7 @@ class Cypher:
 
             encrypted_data = base64.b64encode(''.join(encrypted_data).encode('ascii')).decode('ascii')
 
-            return encrypted_data, key
+            return (encrypted_data, key)
 
         if decrypt:
             inverse_key = {}
@@ -356,7 +370,7 @@ class Cypher:
                 for sym in message:
                     decrypted_data.append(inverse_key.get(sym))
                 
-                return ''.join(decrypted_data), inverse_key
+                return (''.join(decrypted_data), inverse_key)
             except Exception as e:
                 raise ValueError("Monoalphabetic cypher can be decrypted with the exact same key generated during encryption")
     
@@ -376,14 +390,14 @@ class Cypher:
             key = Fernet.generate_key()
             encrypted_data = Fernet(key).encrypt(message.encode('utf-8')).decode('utf-8')
             key = key.decode('utf-8')
-            return encrypted_data, key
+            return (encrypted_data, key)
 
         if decrypt:
             try:
                 key = key.encode('utf-8')
                 decrypted_data = Fernet(key).decrypt(message.encode('utf-8')).decode('utf-8')
                 key = key.decode('utf-8')
-                return decrypted_data, key
+                return (decrypted_data, key)
             except Exception as e:
                 raise ValueError("Fernet cypher can be decrypted with the exact same key generated during encryption")
 
@@ -406,9 +420,97 @@ class Cypher:
         if encrypt:
             data = ''.join([chr(ord(x)^ord(y)) for x, y in zip(message, cycle(key))])
             encrypted_data = (binascii.hexlify(data.encode())).decode()
-            return encrypted_data, key
+            return (encrypted_data, key)
         if decrypt:
             data = (binascii.unhexlify(message.encode())).decode()
             decrypted_data = ''.join([chr(ord(x)^ord(y)) for x, y in zip(data, cycle(key))])
-            return decrypted_data, key
+            return (decrypted_data, key)
+
+    def rsa(self, message: str, encrypt=False, decrypt=False, key=None):
+        """About - RSA (Rivest–Shamir–Adleman) is a public-key cryptosystem that is widely used for secure data transmission.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (Fernet object): Key generated automatically for encryption or decryption.
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
+        if encrypt:
+            if type(key) == rsa.key.PublicKey:
+                pass
+            else:
+                (public_key, private_key) = rsa.newkeys(1024)
+                key = public_key
+            encrypted_data = rsa.encrypt(message.encode('utf-8'), key)
+
+            return (encrypted_data, private_key)
+
+        if decrypt:
+            decrypted_data = rsa.decrypt(message, key).decode('utf-8')
+
+            return (decrypted_data, key)
+
+    def pseudo_random(self, message: str, encrypt=False, decrypt=False, key=None):
+        """Procedure includes shuffling the dictionary with a determined seed.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (int): <int> type key required for encryption or decryption. Defaults to randint(1, 10000).
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
+        if type(key) not in [int, float]:
+            warnings.warn("int based key is preferred for ceaser cypher. Assuming random key.")
+            key = random.randint(1, 10000)
+
+        shuffled_symbols = self.__symbols__.copy()
+        random.Random(key).shuffle(shuffled_symbols)
+
+        if encrypt:                
+            encrypted_data = ''.join([shuffled_symbols[self.__symbols__.index(sym)] for sym in message])
+            return (encrypted_data, int(key))
+
+        if decrypt:
+            decrypted_data = ''.join([self.__symbols__[shuffled_symbols.index(sym)] for sym in message])
+            return (decrypted_data, int(key))
+
+    def vigenere(self, message: str, encrypt=False, decrypt=False, key=None):
+        """Procedure includes encrypting alphabetic text by using a random sample genrated from a seed.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (int): <int> type key required for encryption or decryption. Defaults to randint(1, 10000).
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
+        if type(key) not in [int, float]:
+            warnings.warn("int based key is preferred for vigenere cypher. Assuming random key.")
+            key = random.randint(1, 10000)
+
+        random_symbols = ''.join(random.Random(key).sample(self.__symbols__, k=len(message)))
+
+        if encrypt:
+            encrypted_data = ''
+            for i in range(len(message)) :
+                index = (self.__symbols__.index(message[i]) + self.__symbols__.index(random_symbols[i])) % len(self.__symbols__)
+                encrypted_data += self.__symbols__[index]
+
+            return (encrypted_data, int(key))
+
+        if decrypt:
+            decrypted_data = ''
+            for i in range(len(message)) :
+                index = (self.__symbols__.index(message[i]) - self.__symbols__.index(random_symbols[i])) % len(self.__symbols__)
+                decrypted_data += self.__symbols__[index]
+
+            return (decrypted_data, int(key))
 
