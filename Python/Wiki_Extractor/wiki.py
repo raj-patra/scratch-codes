339dd1936cdfadfe2b84da353a62c532d3d785dd
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-09-21 11:09:21 +0530

    Add wiki search

--- Python/Wiki_Extractor/wiki.py
+++ Python/Wiki_Extractor/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia, argparse
 
-def search_wiki(gist_query, page_query, lang='en'):
+def search_wiki(ping_query, gist_query, page_query, lang='en'):
     response = {}
     try:
         wikipedia.set_lang(lang)
@@ -8,9 +8,12 @@ def search_wiki(gist_query, page_query, lang='en'):
         print({"message": "TypeError: Unsupported language preference passed. Setting default language preference as 'en'", "error": str(e)})
 
     try:
+        if ping_query:
+            topics = wikipedia.search(ping_query)
+            response["topics"] = {"query": ping_query, "data": topics}
         if gist_query:
             gist = wikipedia.summary(gist_query)
-            response["summary"] = {"query": gist_query, "gist": gist}
+            response["summary"] = {"query": gist_query, "data": gist}
 
         print(response)
     except Exception as e:
@@ -19,18 +22,19 @@ def search_wiki(gist_query, page_query, lang='en'):
 
 if __name__ == "__main__":
     my_parser = argparse.ArgumentParser()
-    my_parser.add_argument("--search", action="store", type=str, required=False, help="Gets similar topic for any given query")
+    my_parser.add_argument("--ping", action="store", type=str, required=False, help="Gets similar topics for any given query")
     my_parser.add_argument("--gist", action="store", type=str, required=False, help="Gets summary for a particular topic")
     my_parser.add_argument("--page", action="store", type=str, required=False, help="Gets wiki page for a particular topic")
     my_parser.add_argument("--lang", action="store", type=str, required=False, help="Set language preference for a search instance")
 
     args = my_parser.parse_args()
     
+    ping = args.ping
     gist = args.gist
     page = args.page
     lang = args.lang
     
     if lang:
-        search_wiki(gist, page, lang)
+        search_wiki(ping, gist, page, lang)
     else:
-        search_wiki(gist, page)
+        search_wiki(ping, gist, page)
